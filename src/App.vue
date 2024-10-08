0fb7f4fc69d2d0c5535585d4e6dd4c8ce83c0e88
luni <zoka105105@gmail.com>
2024-10-08 11:26:18 +0800

    bugfix/修改 Accordions

--- src/App.vue
+++ src/App.vue
@@ -5,29 +5,47 @@ import { ref } from 'vue';
 
 const dialogVisible = ref(false)
 
+const dialogPanel = {
+  title: '秋天的散步',
+  content: '秋天的午後，陽光透過樹葉的縫隙灑下金色的光芒，微風輕拂，帶來一絲涼意。沿著小路散步，地面上鋪滿了五顏六色的落葉，有的如紅酒般濃烈，有的則像陽光下的檸檬，閃耀著明亮的黃色。'
+}
 
+const detailList = [{
+  title: '秋日的黃昏',
+  content: '隨著夏天的腳步漸漸遠去，秋天的黃昏帶來了一絲清新的涼意。樹葉在微風中輕輕搖曳，金黃的色彩如同大自然的調色盤，斑駁而美麗。這個季節總是讓人感到特別的平靜。'
+},
+{
+  title: '夜空的星辰',
+  content: '夜幕降臨，天空中繁星閃爍。我坐在院子裡，仰望著無垠的星空，心中感受到一種無法言喻的寧靜。每顆星星都像是遙遠的夢想，閃爍著希望的光芒。此時此刻，煩惱與憂慮都隨著夜風遠去。大自然的壯麗讓我意識到，生活中有太多美好的事物值得我們去珍惜。'
+},
+{
+  title: '書店的秘密',
+  content: '在城市的一角，有一家古老的書店，裡面散發著淡淡的書香。每當我踏進這個小小的世界，時間似乎都靜止了。書架上擺滿了各式各樣的書籍，它們靜靜地講述著不同的故事。我喜歡在角落裡的沙發上坐下，隨意翻閱那些封面上泛黃的書本。每一本書都是一個新世界，帶我踏上未知的旅程。'
+},
+{
+  title: '海邊的回憶',
+  content: '夏天的海邊，海浪輕輕拍打著岸邊，留下白色的泡沫。陽光灑在沙灘上，金色的光輝讓人感到無比溫暖。我和朋友們一起嬉戲，堆沙堡、撿貝殼，笑聲在空中迴蕩。這些美好的回憶將永遠印在心中，成為青春的印記。海邊的每一個瞬間都是那麼珍貴，像是生命中閃耀的星光。'
+},]
 </script>
 
 <template>
   <div class="container">
-    <button class="openDialogButton" @click="dialogVisible = true">
+    <button class="openButton" @click="dialogVisible = true">
       <span>開啟 Dialog</span>
       <span class="material-symbols-outlined openIcon">dialogs</span>
     </button>
 
     <Dialog v-model="dialogVisible">
-      <template #title></template>
-      <template #content></template>
+      <template #title>{{ dialogPanel.title }}</template>
+      <template #content>{{ dialogPanel.content }}</template>
     </Dialog>
 
-
-
-    <Accordions>
-      <template #title={title}>
-        {{ title }}
+    <Accordions v-for="(detail, id) in detailList" :key="detail.id">
+      <template #title>
+        {{ detail.title }}
       </template>
-      <template #content="{ content }">
-        {{ content }}
+      <template #content>
+        {{ detail.content }}
       </template>
     </Accordions>
   </div>
@@ -45,24 +63,21 @@ const dialogVisible = ref(false)
 }
 
 .openButton {
-  color: rgb(250, 250, 250);
-  font: 700 32px '';
-}
-
-.openButton .openIcon {
-  font-size: 36px;
-  padding-left: 20px;
-}
-
-.openDialogButton {
   display: flex;
   justify-content: center;
   align-items: center;
   width: max-content;
   height: max-content;
   padding: 15px 20px;
+  color: rgb(250, 250, 250);
+  font: 700 32px '';
   background-color: rgb(29, 171, 218);
   border-radius: 5px;
   cursor: pointer;
+}
+
+.openButton .openIcon {
+  font-size: 36px;
+  padding-left: 20px;
 }
 </style>
--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,5 +1,9 @@
 <script setup>
 import { ref, watch, onMounted } from 'vue';
+const defaultText = {
+    title: 'Default Title',
+    content: 'Default Content'
+}
 const props = defineProps({
     modelValue: {
         type: Boolean,
@@ -33,17 +37,19 @@ watch(() => props.modelValue, (newValue) => {
 <template>
     <dialog ref="dialog" @click.self="closeDialog">
         <section>
-            <slot name="title">
-                <h1 class="titleText">在小鎮的午後</h1>
-            </slot>
+            <h1 class="titleText">
+                <slot name="title">
+                    {{ defaultText.title }}
+                </slot>
+            </h1>
             <span class="material-symbols-outlined closeIcon" @click="closeDialog">
                 close
             </span>
-            <slot name="content">
-                <p class="contentText">
-                    在一個寧靜的小鎮上，午後的陽光柔和地灑在街道上。樹影斑駁，微風輕拂，帶來了遠處花香的氣息。小鎮的生活彷彿在這樣的時光中變得緩慢而恬靜。我坐在咖啡館的戶外座位上，手中捧著一杯剛沖好的拿鐵，熱氣緩緩上升，彷彿在與午後的陽光對話。咖啡館裡傳來輕柔的音樂，伴隨著周圍人們的低語聲，讓人感到一種難以言喻的安逸。不遠處，一位老奶奶正在市場上擺攤，手中拿著新鮮的水果和自家種的蔬菜。她的臉上洋溢著微笑，與路過的每一個人打著招呼。小孩們則在街道上追逐嬉鬧，他們的笑聲如同清脆的鈴聲，回蕩在空氣中。這樣的午後，不僅僅是時間的流逝，更是一種生活的享受。我放下咖啡杯，靜靜地看著眼前的一切，心中充滿了對生活的感激。每一個細微的瞬間，都彷彿在告訴我：生活的美好，常常藏在那些不經意的時刻裡。就在這時，一位年輕的畫家走進了我的視線，她手裡拿著畫板，似乎要將眼前的景象捕捉下來。她專注的神情讓我想起了創作的熱情，無論是畫畫、寫作還是其他形式的表達，都是對生活的熱愛。
-                </p>
-            </slot>
+            <p class="contentText">
+                <slot name="content">
+                    {{ defaultText.content }}
+                </slot>
+            </p>
         </section>
     </dialog>
 </template>
@@ -77,17 +83,17 @@ section {
     color: brown;
     cursor: pointer;
 }
+
 .titleText {
-  color: chocolate;
+    color: chocolate;
 }
 
 .contentText {
-  padding: 15px 5px 15px 10px;
-  margin: 0 15px;
-  border-top: 1px dotted #000;
-  font: 400 20px '';
-  color: rgb(57, 28, 28);
-  grid-area: 2/1/span 1/span 2;
+    padding: 15px 5px 15px 10px;
+    margin: 0 15px;
+    border-top: 1px dotted #000;
+    font: 400 20px '';
+    color: rgb(57, 28, 28);
+    grid-area: 2/1/span 1/span 2;
 }
-
 </style>
